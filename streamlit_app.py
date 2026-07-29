ab8ac302803bd8f9302c39e8515aaaf109abd8e4
benjaminp-alt <benjamin.printz@gmail.com>
2026-07-29 15:52:40 +0200

    Update streamlit_app.py
    kommenterat bort spårkoden

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ if ingredients_list:
         st.subheader(fruit_chosen + 'Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-        st.write(smoothiefroot_response)
+        # st.write(smoothiefroot_response)
 
     #st.write(ingredients_string)
 
